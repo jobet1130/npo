2b4ae33a519c475b8aa611c10454cee05a9079ea
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-19 21:19:48 +0800

    Initialize

---
+++ npo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-^2_20=_bac#g-n#g3^e8gw66$^p7bi$*)ny@@jcr54@6+n&i8l"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
